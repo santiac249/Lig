9ed8c6e7b445ea4a0aa9b4120eaa7996a43a16a9
Sebastian Sepulveda Patiño <sebas20112322@gmail.com>
2024-05-15 00:06:11 -0500

    Creacion de logueo de usuarios

--- app/src/main/java/com/upb/lig/Login.java
+++ app/src/main/java/com/upb/lig/Login.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,9 +13,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class Login extends AppCompatActivity {
 
+    private EditText emailField;
+    private EditText passwordField;
     private Button btn_login;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,15 +28,16 @@ public class Login extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
 
+        mAuth = FirebaseAuth.getInstance(); // Inicializa la instancia de FirebaseAuth
+
+        emailField = findViewById(R.id.txtMail);
+        passwordField = findViewById(R.id.editTextTextPassword);
         btn_login = findViewById(R.id.btn_login);
+
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Crea un nuevo Intent que apunte a la actividad Login
-                Intent intent = new Intent(Login.this, Espacio.class);
-
-                // Inicia la actividad Login
-                startActivity(intent);
+                validateAndLoginUser();
             }
         });
 
@@ -38,5 +46,32 @@ public class Login extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    private void validateAndLoginUser() {
+        String email = emailField.getText().toString().trim();
+        String password = passwordField.getText().toString().trim();
+
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(Login.this, "Por favor ingresar un correo y contraseña", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        loginUser(email, password);
+    }
+
+    private void loginUser(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        Toast.makeText(Login.this, "Inicio de sesión exitoso", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(Login.this, Espacio.class);
+                        startActivity(intent);
+                        finish();
+                    } else {
+                        String errorMessage = task.getException() != null ? task.getException().getMessage() : "Error desconocido";
+                        Toast.makeText(Login.this, "Autenticación fallida: " + errorMessage, Toast.LENGTH_LONG).show();
+                    }
+                });
     }
 }
