09b30cd9251fb251976a0ab28c8847ecb6ae19b1
Sebastian Sepulveda Patiño <sebas20112322@gmail.com>
2024-05-15 18:32:59 -0500

    Ultimos cambios de vistas y build apk

--- app/src/main/java/com/upb/lig/Servicios.java
+++ app/src/main/java/com/upb/lig/Servicios.java
@@ -1,6 +1,10 @@
 package com.upb.lig;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +14,29 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class Servicios extends AppCompatActivity {
 
+    private ImageButton btn_cerrar;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_servicios);
+        btn_cerrar = findViewById(R.id.btn_cerrar);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.btn_home), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        btn_cerrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Crea un nuevo Intent que apunte a la actividad Creditos
+                Intent intent = new Intent(Servicios.this, Espacio.class);
+                // Inicia la actividad Registro de usuario
+                startActivity(intent);
+
+            }
         });
     }
 }
